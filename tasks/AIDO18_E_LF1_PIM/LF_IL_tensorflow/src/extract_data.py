a066f515c230e0f138a9236ac00aad13b0cd8e6b
lapandic <ldzenan@student.ethz.ch>
2018-11-19 11:43:42 +0100

    reverted db name extraction

--- tasks/AIDO18_E_LF1_PIM/LF_IL_tensorflow/src/extract_data.py
+++ tasks/AIDO18_E_LF1_PIM/LF_IL_tensorflow/src/extract_data.py
@@ -83,7 +83,7 @@ def main():
         bag_ID = file.partition(".bag")[0]
 
         # extract the duckiebot name to complete the definition of the nodes
-        duckiebot_name = "megabot08"#file.partition("_")[2].partition(".bag")[0]
+        duckiebot_name = file.partition("_")[2].partition(".bag")[0]
 
         # complete the topics names with the duckiebot name in the beginning
         ros_topics_temp = copy(ros_topics)
